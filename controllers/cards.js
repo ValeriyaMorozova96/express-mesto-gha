3091d8b3e181483426a92e0e89e4a13bbc3c8e4f
Morozova Valeriya <sandy.lera@yandex.ru>
2023-05-24 23:24:45 +0300

    fixes

--- controllers/cards.js
+++ controllers/cards.js
@@ -57,9 +57,9 @@ module.exports.likeCard = (req, res) => {
     .then((card) => {
       if (!card) {
         res.status(ERROR_NOT_FOUND).send({ message: 'Передан несуществующий _id карточки' });
-        return;
+      } else {
+        res.status(200).send({ data: card });
       }
-      res.status(200).send({ data: card });
     })
     .catch((err) => {
       if (err.name === 'CastError') {
@@ -79,9 +79,9 @@ module.exports.dislikeCard = (req, res) => {
     .then((card) => {
       if (!card) {
         res.status(ERROR_NOT_FOUND).send({ message: 'Передан несуществующий _id карточки' });
-        return;
+      } else {
+        res.status(200).send({ data: card });
       }
-      res.status(200).send({ data: card });
     })
     .catch((err) => {
       if (err.name === 'CastError') {
