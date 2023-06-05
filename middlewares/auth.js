000573a76e8679df79151817c518e9bf3b49e682
Morozova Valeriya <sandy.lera@yandex.ru>
2023-06-06 01:29:29 +0300

    fix4

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,10 +3,10 @@ const AuthorizationError = require('../utils/errors/AuthorizationError');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer')) {
+  if (!authorization || !authorization.startsWith('Bearer ')) {
     throw new AuthorizationError('Необходима авторизация');
   }
-  const token = authorization.replace('Bearer', '');
+  const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, 'some-secret-key');
